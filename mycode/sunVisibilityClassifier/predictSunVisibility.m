e7646bbfd41eaef59a8d1e580f3c626327237d80
Jean-Francois Lalonde <jflalonde@gmail.com>
2011-12-13 17:02:32 -0500

    Sun visibility predictor should be mostly functional.

--- mycode/sunVisibilityClassifier/predictSunVisibility.m
+++ mycode/sunVisibilityClassifier/predictSunVisibility.m
@@ -1,4 +1,5 @@
-function [vis, prob] = predictSunVisibility(img, visibilityClassifier)
+function [vis, prob] = predictSunVisibility(img, visibilityClassifier, ...
+    geomContextInfo, bndInfo, shadowInfo)
 % Predicts the sun visibility in an image.
 %
 % See also:
@@ -11,11 +12,94 @@ visibilityFeaturesOpts = {'GeometricContextArea', 1, 'MeanSkyColor', 1, 'MeanGro
     'MaxGroundCluster', 1, 'MaxWallsCluster', 1, 'SceneContrast', 1, 'SVHistogram', ...
     1, 'LogHistogram', 1, 'GroundShadows', 1};
 
-visibilityFeatures = computeSunVisibilityFeatures(img, visibilityFeaturesOpts{:});     
+% parse the options
+visibilityFeaturesOpts = parseVisibilityFeaturesOpts(visibilityFeaturesOpts, ...
+    geomContextInfo, bndInfo, shadowInfo);
 
-% Test classifier (re-scale the same way as input)
-visibilityFeaturesScaled = scaleFeaturesSVM(visibilityFeatures, ...
+% compute visibility features
+visibilityFeatures = computeSunVisibilityFeatures(img, visibilityFeaturesOpts{:});    
+
+catFeatures = [];
+featuresToLoad = sort(fieldnames(visibilityFeatures));
+for f=1:length(featuresToLoad)
+    % original version
+    tmpFeatures = visibilityFeatures.(featuresToLoad{f});
+    catFeatures = cat(2, catFeatures, tmpFeatures(:)');
+end
+
+% Re-scale features the same way as training data
+visibilityFeaturesScaled = scaleFeaturesSVM(catFeatures, ...
     visibilityClassifier.minVal, visibilityClassifier.scale); 
 
-[vis, d, prob] = svmpredict([], visibilityFeaturesScaled, ...
-    visibilityClassifier.model, '-b 1');
+% Run classifier
+[vis, d, prob] = svmpredict(0, visibilityFeaturesScaled, visibilityClassifier.model, '-b 1');
+
+
+function visibilityFeaturesOpts = parseVisibilityFeaturesOpts(visibilityFeaturesOpts, ...
+    geomContextInfo, bndInfo, shadowInfo)
+% Helper function
+
+% extract masks from geometric context information
+[m,mind] = max(cat(3, geomContextInfo.allGroundMask, geomContextInfo.allWallsMask, geomContextInfo.allSkyMask), [], 3);
+groundMask = mind == 1;
+wallsMask = mind == 2;
+skyMask = mind == 3;
+
+groundProb = geomContextInfo.allGroundMask;
+
+defaultSubArgs = struct('GeometricContextArea', 0, 'MeanSkyColor', 0, 'MeanGroundIntensity', 0, ...
+                        'MaxGroundCluster', 0, 'MaxWallsCluster', 0, 'SceneContrast', 0, 'GroundShadows', ...
+                        0, 'SkyCategory', 0, 'LogHistogram', 0, 'SVHistogram', 0);
+subArgs = parseargs(defaultSubArgs, visibilityFeaturesOpts{:});
+
+if subArgs.GeometricContextArea
+    % load geometric context information
+    visibilityFeaturesOpts = cat(2, visibilityFeaturesOpts, ...
+        {'GroundMask', groundMask, 'WallsMask', wallsMask, 'SkyMask', skyMask});
+end
+
+if subArgs.MeanSkyColor
+    % load the sky mask
+    visibilityFeaturesOpts = cat(2, visibilityFeaturesOpts, ...
+        {'SkyMask', skyMask});
+end
+
+if subArgs.MeanGroundIntensity
+   visibilityFeaturesOpts = cat(2, visibilityFeaturesOpts, ...
+       {'GroundMask', groundMask});
+end
+
+if subArgs.MaxGroundCluster
+    visibilityFeaturesOpts = cat(2, visibilityFeaturesOpts, ...
+        {'GroundProb', groundProb});
+end
+
+if subArgs.MaxWallsCluster
+    visibilityFeaturesOpts = cat(2, visibilityFeaturesOpts, ...
+        {'WallRight', geomContextInfo.wallRight}, ...
+        {'WallLeft', geomContextInfo.wallLeft}, ...
+        {'WallFacing', geomContextInfo.wallFacing});
+end
+
+if subArgs.SceneContrast
+    % load the sky mask
+    visibilityFeaturesOpts = cat(2, visibilityFeaturesOpts, ...
+        {'GroundMask', groundMask, 'WallsMask', wallsMask});
+end
+
+if subArgs.GroundShadows        
+    % load the ground shadows & boundary information    
+    visibilityFeaturesOpts = cat(2, visibilityFeaturesOpts, ...
+        {'ShadowBoundaries', bndInfo.boundaries(shadowInfo.boundaryLabels==0), ...
+        'GroundProb', groundProb});
+end
+
+if subArgs.LogHistogram
+    visibilityFeaturesOpts = cat(2, visibilityFeaturesOpts, ...
+        {'GroundProb', geomContextInfo.allGroundMask});
+end
+
+if subArgs.SVHistogram
+    visibilityFeaturesOpts = cat(2, visibilityFeaturesOpts, ...
+        {'GroundProb', geomContextInfo.allGroundMask});
+end
