0e2801ec9c8e83bc9a6ac3f8e35302b20c6d0350
Jean-Francois Lalonde <jlalonde@tandentvision.com>
2012-09-21 15:44:24 -0400

    Fixed small type bug.

--- mycode/util/interpBoundarySubPixel.m
+++ mycode/util/interpBoundarySubPixel.m
@@ -20,7 +20,8 @@ function [meanBndInterpVal, bndInterpVal] = interpBoundarySubPixel(boundaries, i
 
 % compute probability of ground at each boundary point
 boundariesCat = max(cat(1, boundaries{:}), 1);
-boundariesCatInterpVal = interp2(1:size(interpMap,2), 1:size(interpMap,1), interpMap, min(boundariesCat(:,1), size(interpMap,2)), min(boundariesCat(:,2), size(interpMap,1)));
+boundariesCatInterpVal = interp2(1:size(interpMap,2), 1:size(interpMap,1), ...
+    double(interpMap), min(boundariesCat(:,1), size(interpMap,2)), min(boundariesCat(:,2), size(interpMap,1)));
 
 % compute mean for each boundary line
 boundariesLength = cellfun(@(x) length(x), boundaries);
